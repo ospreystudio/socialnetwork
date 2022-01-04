a80bc94d7e2e2284670582820090b09c2968ae5b
ospreystudio <64425019+ospreystudio@users.noreply.github.com>
2022-01-04 12:49:38 +0400

    login complite

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,12 +7,39 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Laravel\Passport\Client;
+
 
 class AuthController extends Controller
 {
-    public function login() {
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required |string | email | max:255',
+            'password' => 'required | between:8, 255 |',
+        ]);
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
+        }
 
+        $passwordGrantClient = Client::where('password_client', 1)->first();
+
+        $data = [
+            'grant_type' => 'password',
+            'client_id' => $passwordGrantClient->id,
+            'client_secret' => $passwordGrantClient->secret,
+            'username' => $request->email,
+            'password' => $request->password,
+            'scope' => '*'
+        ];
+
+        $tokenRequest = Request::create('/oauth/token', 'post', $data);
+
+        return app() -> handle($tokenRequest);
     }
+
+
+
 
     public function register(Request $request) {
 
